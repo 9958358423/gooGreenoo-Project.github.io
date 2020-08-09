dd0ab3430d6cabda172683da0dfc29c82ca654cf
unknown <goelritik31@gmail.com>
2020-08-09 19:13:21 +0530

    'changes'

---
+++ css/scripts.js
@@ -0,0 +1,32 @@
+function myFunction()
+{
+ location.replace("login.html")
+}
+function myFun()
+{
+ location.replace("sign.html")
+}
+function Home()
+{
+ location.replace("index.html")
+}
+function Fruits()
+{
+ location.replace("Fruits.html")
+}
+function Vegetables()
+{
+ location.replace("Veg.html")
+}
+function Non()
+{
+ location.replace("nonveg.html")
+}
+function More()
+{
+ location.replace("More.html")
+}
+function About()
+{
+ location.replace("About.html")
+}
